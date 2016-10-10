014747c8fd4ce6b2417354fa64fc995084d8c19d
Yury Snegirev <yusnegirev@at-consulting.ru>
2016-10-10 11:59:01 +0300

    Fixed definition of callback

--- trb-engine/app/concepts/trb/engine/message/callback.rb
+++ trb-engine/app/concepts/trb/engine/message/callback.rb
@@ -1,11 +1,13 @@
 require "disposable/callback"
 
-module Trb::Engine::Message::Callback
-  class AfterSave < Disposable::Callback::Group
-    on_create :do_something!
+module Trb::Engine
+  module Message::Callback
+    class AfterSave < Disposable::Callback::Group
+      on_create :do_something!
 
-    def do_something!(contract, options)
-      # do something...
+      def do_something!(contract, options)
+        # do something...
+      end
     end
   end
 end
--- trb-engine/app/concepts/trb/engine/message/create.rb
+++ trb-engine/app/concepts/trb/engine/message/create.rb
@@ -1,11 +1,13 @@
-class Trb::Engine::Message::Create < Trailblazer::Operation
-  include Model, Callback
-  model Trb::Engine::Message, :create
+module Trb::Engine
+  class Message::Create < Trailblazer::Operation
+    include Model, Callback
+    model Trb::Engine::Message, :create
 
-  callback :after_save, Trb::Engine::Message::Callback
+    callback :after_save, Trb::Engine::Message::Callback::AfterSave
 
-  def process(params)
-    @model.save
-    callback! :after_save
+    def process(params)
+      @model.save
+      callback! :after_save
+    end
   end
 end
